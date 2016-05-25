bf87aad1b49ae166a765190b35baffda9cad4687
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-05-25 23:39:16 +0300

    Add frontend module to the project #T4U-23 Fixed

---
+++ trip4u-frontend/app/app.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Author: Mary Kuchumova
+ * Date: 25 май 2016
+ * Time: 21:46
+ */
+
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Aloha :)</h1>'
+})
+export class AppComponent { }
---
+++ trip4u-frontend/app/main.ts
@@ -0,0 +1,10 @@
+/**
+ * Author: Mary Kuchumova
+ * Date: 25 май 2016
+ * Time: 23:17
+ */
+
+
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
