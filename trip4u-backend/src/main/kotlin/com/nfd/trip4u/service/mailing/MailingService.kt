49cd534f96a11f1c68b52145b20ecdc9708baab3
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-05-27 14:55:37 +0300

    Add sending plain text email #T4U-27

---
+++ trip4u-backend/src/main/kotlin/com/nfd/trip4u/service/mailing/MailingService.kt
@@ -0,0 +1,32 @@
+package com.nfd.trip4u.service.mailing
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.stereotype.Service
+
+/**
+ * Author: Mary Kuchumova
+ * Date: 27 Май 2016
+ * Time: 14:49
+ */
+
+@Service
+open class MailingService {
+
+    @Autowired
+    lateinit var mailSender: JavaMailSender
+
+    open fun sendPlainTextEmail(subject: String, recipient: String, messageBody: String) {
+
+        val mimeMessage = mailSender.createMimeMessage()
+        val message = MimeMessageHelper(mimeMessage, "UTF-8")
+        message.setSubject(subject)
+        message.setFrom("night.fury.devs@gmail.com")
+        message.setTo(recipient)
+        message.setText(messageBody);
+
+        this.mailSender.send(mimeMessage);
+    }
+
+}
