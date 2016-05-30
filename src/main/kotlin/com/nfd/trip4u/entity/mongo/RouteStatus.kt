161235766316b9233e4e86b451c2bcc6a44eb513
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-05-30 17:20:28 +0300

    Add route document #T4U-17

---
+++ src/main/kotlin/com/nfd/trip4u/entity/mongo/RouteStatus.kt
@@ -0,0 +1,13 @@
+package com.nfd.trip4u.entity.mongo
+
+/**
+ * Author: Mary Kuchumova
+ * Date: 30 May 2016
+ * Time: 17:19
+ */
+
+enum class RouteStatus(val value: String){
+    VISITED("visited"),
+    PLANNING_TO_VISIT("planning_to_visit"),
+    IN_PROGRESS("in_progress")
+}
