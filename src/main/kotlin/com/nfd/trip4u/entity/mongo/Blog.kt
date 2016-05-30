69d489e3f6bda3f31ac2ff0fd2c538258f123d5b
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-05-30 17:03:28 +0300

    Add blog document #T4U-17

---
+++ src/main/kotlin/com/nfd/trip4u/entity/mongo/Blog.kt
@@ -0,0 +1,35 @@
+package com.nfd.trip4u.entity.mongo
+
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
+import java.util.*
+
+/**
+ * Author: Mary Kuchumova
+ * Date: 30 May 2016
+ * Time: 16:59
+ */
+
+@Document(collection = "Blog")
+data class Blog(
+        var id: Long,
+        var title: String,
+
+        @Field(value = "user_id")
+        var userId: Long,
+
+        @Field(value = "start_date")
+        var startDate: Date,
+
+        @Field(value = "end_date")
+        var endDate: Date,
+
+        var tags: List<String>,
+        var status: VerificationStatus,
+        var rating: List<RatingEntry>,
+
+        @Field(value = "route_id")
+        var routeId: Long,
+
+        var comments: List<Comment>
+        )
