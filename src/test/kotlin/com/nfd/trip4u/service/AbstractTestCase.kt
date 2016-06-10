85a1f6ce81ccc447e95d65f13da80130df0f2181
Alexey Kleschikov <alex.vesy96@gmail.com>
2016-06-10 19:56:27 +0300

    Fix test running.

--- src/test/kotlin/com/nfd/trip4u/service/AbstractTestCase.kt
+++ src/test/kotlin/com/nfd/trip4u/service/AbstractTestCase.kt
@@ -3,7 +3,6 @@ package com.nfd.trip4u.service
 import com.nfd.trip4u.Application
 import org.junit.runner.RunWith
 import org.springframework.boot.test.SpringApplicationConfiguration
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
 /**
@@ -14,5 +13,4 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
 @RunWith(SpringJUnit4ClassRunner::class)
 @SpringApplicationConfiguration(classes = arrayOf(Application::class))
-@ActiveProfiles("dev")
-abstract  class AbstractTestCase
+abstract class AbstractTestCase
