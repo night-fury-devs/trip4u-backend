a3ba7d7bc29a25f19160141ec0c22e3e80a3c593
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-06-17 22:24:16 +0300

    Fix service method signature #T4U-38

--- src/main/kotlin/com/nfd/trip4u/service/domain/UserServiceImpl.kt
+++ src/main/kotlin/com/nfd/trip4u/service/domain/UserServiceImpl.kt
@@ -1,8 +1,7 @@
-package com.nfd.trip4u.service.domain.impl
+package com.nfd.trip4u.service.domain
 
 import com.nfd.trip4u.entity.domain.User
 import com.nfd.trip4u.repository.domain.UserRepository
-import com.nfd.trip4u.service.domain.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.domain.Page
@@ -16,45 +15,45 @@ import java.awt.print.Pageable
  */
 
 @Service
-open class UserServiceImpl : UserService {
+open class UserServiceImpl {
 
 //    @Autowired
 //    @Qualifier("userRepository")
 //    lateinit var userRepository: UserRepository
 
-    override fun save(user: User): User {
+    fun save(user: User): User {
         throw UnsupportedOperationException()
     }
 
-    override fun delete(user: User) {
+    fun delete(user: User) {
         throw UnsupportedOperationException()
     }
 
-    override fun findAll(): Iterable<User> {
+    fun findAll(): Iterable<User> {
         throw UnsupportedOperationException()
     }
 
-    override fun findAll(pageable: Pageable): Page<User> {
+    fun findAll(pageable: Pageable): Page<User> {
         throw UnsupportedOperationException()
     }
 
-    override fun findById(id: Long): User {
+    fun findById(id: Long): User {
         throw UnsupportedOperationException()
     }
 
-    override fun findByUserName(userName: String): List<User> {
+    fun findByUserName(userName: String): User {
         throw UnsupportedOperationException()
     }
 
-    override fun findByEmail(email: String): List<User> {
+    fun findByEmail(email: String): User {
         throw UnsupportedOperationException()
     }
 
-    override fun findByUserNameAndPassword(userName: String, password: String): List<User> {
+    fun findByUserNameAndPassword(userName: String, password: String): User {
         throw UnsupportedOperationException()
     }
 
-    override fun count(): Long {
+    fun count(): Long {
         throw UnsupportedOperationException()
     }
 
