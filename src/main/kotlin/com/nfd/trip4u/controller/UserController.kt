cf3d02dbf3e729515f268414aa705e7e40bd96bf
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-06-19 19:56:19 +0300

    Add method providing registration of new user #T4U-41

--- src/main/kotlin/com/nfd/trip4u/controller/UserController.kt
+++ src/main/kotlin/com/nfd/trip4u/controller/UserController.kt
@@ -1,10 +1,10 @@
 package com.nfd.trip4u.controller
 
+import com.nfd.trip4u.entity.domain.User
 import com.nfd.trip4u.service.domain.UserService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 /**
  * Author: Mary Kuchumova
@@ -19,13 +19,27 @@ open class UserController {
     @Autowired
     private lateinit var userService: UserService
 
+    @RequestMapping(value = "/register", method = arrayOf(RequestMethod.POST))
+    @ResponseStatus(value = HttpStatus.OK)
+    open fun register(userToCreate: User): HttpStatus {
+        if(userService.findByEmail(userToCreate.email) != null || userService.findByUserName(userToCreate.userName) != null) {
+            return HttpStatus.BAD_REQUEST
+        }
+        userService.save(userToCreate)
+        //TODO: add sending verification email to registered user
+        return HttpStatus.CREATED
+    }
+
     @RequestMapping(value = "/validateLogin", method = arrayOf(RequestMethod.GET))
+    @ResponseBody
     open fun validateLogin(login: String): Boolean {
         return userService.findByUserName(login) != null
     }
 
     @RequestMapping(value = "/validateEmail", method = arrayOf(RequestMethod.GET))
+    @ResponseBody
     open fun validateEmai(email: String): Boolean {
         return userService.findByEmail(email) != null
     }
+
 }
