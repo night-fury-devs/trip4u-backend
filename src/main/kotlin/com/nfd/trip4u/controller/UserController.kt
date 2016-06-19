845f1117836c2c1f8014634b73fa1e20b8459292
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-06-20 00:15:04 +0300

    Add server side validation for user registration #T4U-41

--- src/main/kotlin/com/nfd/trip4u/controller/UserController.kt
+++ src/main/kotlin/com/nfd/trip4u/controller/UserController.kt
@@ -1,10 +1,16 @@
 package com.nfd.trip4u.controller
 
+import com.nfd.trip4u.controller.validation.ValidatedUser
+import com.nfd.trip4u.entity.domain.Gender
+import com.nfd.trip4u.entity.domain.Role
 import com.nfd.trip4u.entity.domain.User
 import com.nfd.trip4u.service.domain.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
+import java.util.*
+import javax.validation.Valid
 
 /**
  * Author: Mary Kuchumova
@@ -21,13 +27,22 @@ open class UserController {
 
     @RequestMapping(value = "/register", method = arrayOf(RequestMethod.POST))
     @ResponseStatus(value = HttpStatus.OK)
-    open fun register(userToCreate: User): HttpStatus {
-        if(userService.findByEmail(userToCreate.email) != null || userService.findByUserName(userToCreate.userName) != null) {
+    open fun register(@Valid @RequestBody user: ValidatedUser,
+                      bindingResult: BindingResult): HttpStatus {
+        if (!bindingResult.hasErrors()) {
+            if (userService.findByEmail(user.email) != null ||
+                    userService.findByUserName(user.userName) != null) {
+                return HttpStatus.BAD_REQUEST
+            }
+            val domainUserEntity = User(null, user.userName, user.email, user.password, null,
+                    user.lastName, user.firstName, null, Gender.NOT_DEFINED, null,
+                    ArrayList<Role>(Arrays.asList(Role.USER)))
+            userService.save(domainUserEntity)
+            //TODO: add sending verification email to registered user
+            return HttpStatus.CREATED
+        } else {
             return HttpStatus.BAD_REQUEST
         }
-        userService.save(userToCreate)
-        //TODO: add sending verification email to registered user
-        return HttpStatus.CREATED
     }
 
     @RequestMapping(value = "/validateLogin", method = arrayOf(RequestMethod.GET))
---
+++ src/main/kotlin/com/nfd/trip4u/controller/validation/ValidatedUser.kt
@@ -0,0 +1,32 @@
+package com.nfd.trip4u.controller.validation
+
+import org.hibernate.validator.constraints.Email
+import org.hibernate.validator.constraints.NotEmpty
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
+
+/**
+ * Author: Mary Kuchumova
+ * Date: 19 Jun 2016
+ * Time: 20:02
+ */
+
+open class ValidatedUser {
+
+    @Size(min = 3, max = 30)
+    @NotEmpty
+    lateinit var userName: String
+
+    @NotEmpty
+    @Email
+    lateinit var email: String
+
+    @Pattern(regexp = "(?=^.{8,}$)(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*()]*$")
+    lateinit var password: String
+
+    @Size(max = 30)
+    lateinit var lastName: String
+
+    @Size(max = 30)
+    lateinit var firstName: String
+}
