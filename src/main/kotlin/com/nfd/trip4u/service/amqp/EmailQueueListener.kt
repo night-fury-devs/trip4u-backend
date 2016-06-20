48af06e0c57531c91378ec16098f000d5a692b30
Alexey Kleschikov <alex.vesy96@gmail.com>
2016-06-20 22:18:48 +0300

    Code refactoring in amqp services.

--- src/main/kotlin/com/nfd/trip4u/service/amqp/EmailQueueListener.kt
+++ src/main/kotlin/com/nfd/trip4u/service/amqp/EmailQueueListener.kt
@@ -3,7 +3,6 @@ package com.nfd.trip4u.service.amqp
 import com.nfd.trip4u.configuration.QUEUE_NAME
 import com.nfd.trip4u.entity.mailing.Email
 import com.nfd.trip4u.service.mailing.MailingService
-import org.apache.commons.logging.LogFactory
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.handler.annotation.Payload
@@ -16,17 +15,13 @@ import org.springframework.stereotype.Component
  */
 
 @Component
-open class EmailAmqpListener {
-
-    private val logger = LogFactory.getLog(this.javaClass)
+open class EmailQueueListener {
 
     @Autowired
     private lateinit var mailingService: MailingService
 
     @RabbitListener(queues = arrayOf(QUEUE_NAME))
     open fun processEmail(@Payload message: Email) {
-        logger.debug("Received message $message.")
-
         mailingService.sendMessage(message)
     }
 }
--- src/main/kotlin/com/nfd/trip4u/service/amqp/EmailQueueService.kt
+++ src/main/kotlin/com/nfd/trip4u/service/amqp/EmailQueueService.kt
@@ -2,7 +2,6 @@ package com.nfd.trip4u.service.amqp
 
 import com.nfd.trip4u.configuration.TOPIC_NAME
 import com.nfd.trip4u.entity.mailing.Email
-import org.apache.commons.logging.LogFactory
 import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.converter.MessageConverter
@@ -15,9 +14,7 @@ import org.springframework.stereotype.Service
  */
 
 @Service
-open class EmailAmqpProducer {
-
-    private val logger = LogFactory.getLog(this.javaClass)
+open class EmailQueueService {
 
     @Autowired
     private lateinit var rabbitTemplate: RabbitMessagingTemplate
@@ -26,10 +23,7 @@ open class EmailAmqpProducer {
     private lateinit var jsonConverter: MessageConverter
 
     fun sendMessage(email: Email) {
-        logger.debug("Send message $email")
-
         rabbitTemplate.messageConverter = jsonConverter
-
         rabbitTemplate.convertAndSend(TOPIC_NAME, TOPIC_NAME, email)
     }
 }
