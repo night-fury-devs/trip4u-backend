4097b7e095ccc5d40e2829b944b569f99c026502
Alexey Kleschikov <alex.vesy96@gmail.com>
2016-06-21 21:31:31 +0300

    Transfer registration method to auth endpoint. #T4U-44

--- src/main/kotlin/com/nfd/trip4u/controller/AuthenticationController.kt
+++ src/main/kotlin/com/nfd/trip4u/controller/AuthenticationController.kt
@@ -1,9 +1,16 @@
 package com.nfd.trip4u.controller
 
+import com.nfd.trip4u.controller.validation.ValidatedUser
 import com.nfd.trip4u.service.security.AuthenticationService
+import org.apache.commons.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 /**
  * Author: Alexey Kleschikov
@@ -14,11 +21,29 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(AUTH_PREFIX)
 class AuthenticationController {
 
+    private val log = LogFactory.getLog(this.javaClass)
+
     @Autowired
     private lateinit var authService: AuthenticationService
 
     @RequestMapping(AUTH_CONFIRM)
     fun confirm(id: String): Boolean {
         return authService.confirm(id)
+    }
+
+    @RequestMapping(AUTH_REGISTER)
+    fun register(@Valid @RequestBody user: ValidatedUser, bindingResult: BindingResult): HttpStatus {
+        if (bindingResult.hasErrors()) {
+            return HttpStatus.BAD_REQUEST
+        }
+
+        try {
+            authService.register(user)
+        } catch (ex: BadCredentialsException) {
+            log.warn("Can't register user due to an error.", ex)
+            return HttpStatus.BAD_REQUEST
+        }
+
+        return HttpStatus.CREATED
     }
 }
--- src/main/kotlin/com/nfd/trip4u/controller/UserController.kt
+++ src/main/kotlin/com/nfd/trip4u/controller/UserController.kt
@@ -1,16 +1,10 @@
 package com.nfd.trip4u.controller
 
-import com.nfd.trip4u.controller.validation.ValidatedUser
 import com.nfd.trip4u.entity.domain.User
 import com.nfd.trip4u.service.domain.UserService
-import com.nfd.trip4u.service.security.AuthenticationService
 import org.apache.commons.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
-import org.springframework.security.authentication.BadCredentialsException
-import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
-import javax.validation.Valid
 
 /**
  * Author: Mary Kuchumova
@@ -26,27 +20,6 @@ open class UserController {
 
     @Autowired
     private lateinit var userService: UserService
-
-    @Autowired
-    private lateinit var authService: AuthenticationService
-
-    @RequestMapping(value = "/register", method = arrayOf(RequestMethod.POST))
-    @ResponseStatus(value = HttpStatus.OK)
-    open fun register(@Valid @RequestBody user: ValidatedUser,
-                      bindingResult: BindingResult): HttpStatus {
-        if (bindingResult.hasErrors()) {
-            return HttpStatus.BAD_REQUEST
-        }
-
-        try {
-            authService.register(user)
-        } catch (ex: BadCredentialsException) {
-            log.warn("Can't register user due to an error.", ex)
-            return HttpStatus.BAD_REQUEST
-        }
-
-        return HttpStatus.CREATED
-    }
 
     @RequestMapping(value = "/isLoginAvailable", method = arrayOf(RequestMethod.GET))
     @ResponseBody
@@ -78,5 +51,4 @@ open class UserController {
     open fun token(@RequestParam userName: String, @RequestParam password: String){
         //TODO: add implementation
     }
-
 }
