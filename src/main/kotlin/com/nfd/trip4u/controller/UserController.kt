30528d45af660203b3c34fe5077130ddde25c6ab
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-06-23 15:07:56 +0300

    Remove @ResponseBody  annotation from rest controllers

--- src/main/kotlin/com/nfd/trip4u/controller/UserController.kt
+++ src/main/kotlin/com/nfd/trip4u/controller/UserController.kt
@@ -22,32 +22,27 @@ open class UserController {
     private lateinit var userService: UserService
 
     @RequestMapping(value = "/isLoginAvailable", method = arrayOf(RequestMethod.GET))
-    @ResponseBody
     open fun validateLogin(@RequestParam login: String): Boolean {
         return userService.findByUserName(login) == null
     }
 
     @RequestMapping(value = "/isEmailAvailable", method = arrayOf(RequestMethod.GET))
-    @ResponseBody
     open fun validateEmail(@RequestParam email: String): Boolean {
         return userService.findByEmail(email) == null
     }
 
     @RequestMapping(value = "/{id}", method = arrayOf(RequestMethod.GET))
-    @ResponseBody
     open fun getUserInfo(@PathVariable id: String): User? {
         return userService.findById(id)
     }
 
     @RequestMapping(method = arrayOf(RequestMethod.GET))
-    @ResponseBody
     open fun getUserInfoByToken(@RequestParam token: String): User? {
         //TODO: add token based implementation
         return null
     }
 
     @RequestMapping(value = "/token", method = arrayOf(RequestMethod.GET))
-    @ResponseBody
     open fun token(@RequestParam userName: String, @RequestParam password: String){
         //TODO: add implementation
     }
