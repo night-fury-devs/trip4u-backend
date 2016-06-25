1aa4f7dc9dd15a67cd72dd8bfd579c5836d61ec8
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-06-26 00:11:21 +0300

    Add ehcache configuration #T4U-64 In Progress

---
+++ src/main/kotlin/com/nfd/trip4u/configuration/EhcacheConfiguration.kt
@@ -0,0 +1,37 @@
+package com.nfd.trip4u.configuration
+
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.ehcache.EhCacheCacheManager
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+
+/**
+ * Author: Mary Kuchumova
+ * Date: 26 Jun 2016
+ * Time: 00:04
+ */
+
+@Configuration
+@EnableCaching
+@ComponentScan(value = "com.nfd.trip4u.*")
+open class EhcacheConfiguration{
+
+    @Bean
+    open fun cacheManager(): CacheManager {
+        return EhCacheCacheManager(ehCacheCacheManager().getObject());
+    }
+
+    @Bean
+    open fun ehCacheCacheManager(): EhCacheManagerFactoryBean {
+        var cmfb = EhCacheManagerFactoryBean();
+        cmfb.setConfigLocation(ClassPathResource("ehcache.xml"));
+        cmfb.setShared(true);
+        return cmfb;
+    }
+
+
+}
