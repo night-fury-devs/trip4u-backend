8e74e97f4f1cfd5953d2336a2fd39cadfebbd333
Alexey Kleschikov <alex.vesy96@gmail.com>
2016-07-25 21:37:37 +0300

    Add start and end date to Trip entity.

--- src/main/kotlin/com/nfd/trip4u/entity/domain/Trip.kt
+++ src/main/kotlin/com/nfd/trip4u/entity/domain/Trip.kt
@@ -1,5 +1,8 @@
 package com.nfd.trip4u.entity.domain
 
+import org.springframework.data.mongodb.core.mapping.Field
+import java.util.*
+
 /**
  * Author: Mary Kuchumova
  * Date: 06 July 2016
@@ -8,5 +11,11 @@ package com.nfd.trip4u.entity.domain
 
 data class Trip(
         var routeId: String,
-        var status: RouteStatus
+        var status: RouteStatus,
+
+        @Field(value = "start_date")
+        var startDate: Date? = null,
+
+        @Field(value = "end_date")
+        val endDate: Date? = null
 )
